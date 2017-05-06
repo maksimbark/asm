acefde141cdb8401c034b22e5c8a5e0fb7965065
maksimbarkalov <maks@fizmatspb.ru>
2017-05-07 02:15:34 +0300

    update

--- CacheLab/main.cpp
+++ CacheLab/main.cpp
@@ -7,7 +7,8 @@ int main() {
     int m, l;
     int now;
     //прогон
-    for (int i = 6000; i <= 1050000; i += 1000) {
+
+    for (int i = 6000; i <= 1050000; i += 10000) {
         vector<int> arr(i);
         for (m = 0; m < i; m++) {
             now = arr[m];
@@ -24,29 +25,39 @@ int main() {
     //прямой обход
 
     ofstream cout("outLeftToRight.txt");
-    for (int i = 6000; i <= 1050000; i += 1000) {
-        vector<int> arr(i);
+    for (int i = 6000; i <= 1050000; i += 10000) {
+        register int *arr = (int *)malloc(i * sizeof(int));
+
+        vector<int> way(i);
+        for (int k = 0; k < i; k++) {
+            way[k] = k;
+        }
+        //random_shuffle(way.begin(), way.end());
         megaSum = 0;
-        for (l = 0; l < 15; l++) {
+        for (l = 0; l < 3; l++) {
             auto time = clock();
             for (m = 0; m < i; m++) {
-                now = arr[m];
+                now = arr[way[m]];
+                now++;
+                cerr << now << " " << i << endl;
             }
             megaSum += double((clock() - time)) / double(i);
         }
-        megaSum = megaSum / 15.0;
+        megaSum = megaSum / 3.0;
         cout << i << " " << megaSum << endl;
-        arr.clear();
+        way.clear();
+        free(arr);
 
     }
     cout.close();
     //прогон2
-    for (int i = 6000; i <= 1050000; i += 1000) {
-        vector<int> arr(i);
+    for (int i = 6000; i <= 1050000; i += 10000) {
+
+        register int *arr = (int *)malloc(i * sizeof(int));
         for (m = 0; m < i; m++) {
             now = arr[m];
         }
-        arr.clear();
+       free(arr);
         cerr << "ok2 " << i << " elements" << endl;
     }
 
@@ -54,52 +65,66 @@ int main() {
 
     cout.open("outRightToLeft.txt");
 
-    for (int i = 6000; i <= 1050000; i += 1000) {
-        vector<int> arr(i);
+    for (int i = 6000; i <= 1050000; i += 10000) {
+        register int *arr = (int *)malloc(i * sizeof(int));
+        vector<int> way(i);
+        for (int k = 0; k < i; k++) {
+            way[k] = k;
+        }
+        reverse(way.begin(), way.end());
         megaSum = 0;
-        for (l = 0; l < 15; l++) {
+        for (l = 0; l < 3; l++) {
             auto time = clock();
             for (m = i - 1; m >= 0; m--) {
-                now = arr[m];
+                now = arr[way[m]];
+                now++;
+                cerr << now << " " << i << endl;
             }
             megaSum += double((clock() - time)) / double(i);
         }
-        megaSum = megaSum / 15.0;
+        megaSum = megaSum / 3.0;
         cout << i << " " << megaSum << endl;
-        arr.clear();
+        free(arr);
+        way.clear();
 
     }
     cout.close();
     //прогон3
-    for (int i = 6000; i <= 1050000; i += 1000) {
-        vector<int> arr(i);
+    for (int i = 6000; i <= 1050000; i += 10000) {
+        register int *arr = (int *)malloc(i * sizeof(int));
         for (m = 0; m < i; m++) {
             now = arr[m];
+
         }
-        arr.clear();
+      free(arr);
         cerr << "ok2 " << i << " elements" << endl;
     }
 
     cerr << "meow3";
     cout.open("outRandom.txt");
 
-    for (int i = 6000; i <= 1050000; i += 1000) {
-        vector<int> arr(i);
+    for (int i = 6000; i <= 1050000; i += 10000) {
+        register int *arr = (int *)malloc(i * sizeof(int));
+        vector<int> way(i);
+        for (int k = 0; k < i; k++) {
+            way[k] = k;
+        }
+        random_shuffle(way.begin(), way.end());
         megaSum = 0;
-        for (l = 0; l < 15; l++) {
+        for (l = 0; l < 3; l++) {
             auto time = clock();
-
-                for (m = 0; m < i / 2; m++) {
-                    now = arr[m];
-                    now = arr[i - 1 - m];
-                }
-                now = arr[i / 2];
-            megaSum += double((clock() - time)) / double(i);
+            for (m = 0; m < i; m++) {
+                now = arr[way[m]];
+                now++;
+                cerr << now << " " << i << endl;
             }
-        megaSum = megaSum / 15.0;
+            megaSum += double((clock() - time)) / double(i);
+        }
+        megaSum = megaSum / 3.0;
 
         cout << i << " " << megaSum << endl;
-        arr.clear();
+        free(arr);
+        way.clear();
 
     }
 
